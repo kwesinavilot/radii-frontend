edebc62683a12ed344e05a41b9b8ef94b74a6fd6
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-06-02 07:55:55 +0000

    Style the forgot password

--- app/(auth)/forgotpassword/page.tsx
+++ app/(auth)/forgotpassword/page.tsx
@@ -86,7 +86,7 @@ const ForgotPassword: React.FC = () => {
                         id="email"
                         value={email}
                         onChange={handleChange}
-                        className="bg-gray-50 border border-gray-500 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-white-700 dark:bg-white-600 dark:placeholder-gray-400 dark:text-gray-800 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 border border-[#33a79c] text-gray-900 sm:text-sm rounded-lg focus:ring-[#33a79c] focus:border-[#33a79c] block w-full p-2.5 dark:bg-white-700 dark:bg-white-600 dark:placeholder-gray-400 dark:text-gray-800 dark:focus:ring-[#33a79c]"
                         placeholder="johndoe@gmail.com"
                         required
                       />
