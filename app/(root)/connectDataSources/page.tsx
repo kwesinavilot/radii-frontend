11e402c570a38f71bfcc1454c4a9fb2cafd5adc2
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-06-03 21:30:04 +0000

    Complete the connect data sources mockup

--- app/(root)/connectDataSources/page.tsx
+++ app/(root)/connectDataSources/page.tsx
@@ -6,134 +6,137 @@ import { MdOutlineArrowBackIosNew } from "react-icons/md";
 
 const ConnectDataSource: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="h-screen overflow-hidden bg-gray-100">
       <Navbar
         title="Connect a Data Source"
         icon={<MdOutlineArrowBackIosNew />}
       />
-      <div className="container w-4/6 p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-          <div className="col-span-1 sm:col-span-3">
-            <h2 className="text-[18px] font-bold mb-4">Databases</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/postgres.png"
-                    alt="postgres"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  PostgreSQL
+
+      <div className="h-full sm:col-span-3 py-4 m-4 sm:px-16 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-300 ">
+        <div className="container w-4/6 p-4">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            <div className="col-span-1 sm:col-span-3">
+              <h2 className="text-[18px] font-bold mb-4">Databases</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/postgres.png"
+                      alt="postgres"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    PostgreSQL
+                  </div>
                 </div>
-              </div>
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/mysql.png"
-                    alt="mysql"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  MySQL
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/mysql.png"
+                      alt="mysql"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    MySQL
+                  </div>
                 </div>
-              </div>
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/snowflakes.png"
-                    alt="snowflakes"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  Snowflakes
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/snowflakes.png"
+                      alt="snowflakes"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    Snowflakes
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-span-1 sm:col-span-3">
-            <h2 className="text-[18px] font-bold mb-4">Radii Hosted</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/csv.png"
-                    alt="csv"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  CSV
+            <div className="col-span-1 sm:col-span-3">
+              <h2 className="text-[18px] font-bold mb-4">Radii Hosted</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/csv.png"
+                      alt="csv"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    CSV
+                  </div>
                 </div>
-              </div>
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/pdf2.png"
-                    alt="pdf"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  PDF
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/pdf2.png"
+                      alt="pdf"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    PDF
+                  </div>
                 </div>
-              </div>
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/docs.png"
-                    alt="docs"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  DOCS
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/docs.png"
+                      alt="docs"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    DOCS
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div className="col-span-1 sm:col-span-3">
-            <h2 className="text-[18px] font-bold mb-4">Others</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/drive.png"
-                    alt="drive"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  Google Drive
+            <div className="col-span-1 sm:col-span-3">
+              <h2 className="text-[18px] font-bold mb-4">Others</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/drive.png"
+                      alt="drive"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    Google Drive
+                  </div>
                 </div>
-              </div>
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/qb.png"
-                    alt="qb"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  Quickbooks
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/qb.png"
+                      alt="qb"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    Quickbooks
+                  </div>
                 </div>
-              </div>
-              <div className="dataSourceBtn">
-                <div className="inner rounded-lg shadow hover:bg-gray-50">
-                  <Image
-                    src="/airtable.png"
-                    alt="airtable"
-                    width={32}
-                    height={32}
-                    className="mr-2"
-                  />
-                  Airtable
+                <div className="dataSourceBtn">
+                  <div className="inner rounded-lg shadow hover:bg-gray-50">
+                    <Image
+                      src="/airtable.png"
+                      alt="airtable"
+                      width={32}
+                      height={32}
+                      className="mr-2"
+                    />
+                    Airtable
+                  </div>
                 </div>
               </div>
             </div>
