e3fd86adeb118246a68352ec1727fea886973cbf
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-06-07 18:01:16 +0000

    work on the card sizes

--- app/component/Card.tsx
+++ app/component/Card.tsx
@@ -1,18 +1,39 @@
-import { FC } from "react";
+// import { FC } from "react";
+
+// interface CardProps {
+//   title: string;
+//   onClick: () => void;
+// }
+
+// const Card: FC<CardProps> = ({ title, onClick }) => {
+//   return (
+//     <button
+//       onClick={onClick}
+//       className="p-4 bg-white border rounded-lg shadow-md hover:bg-gray-100 transition-all"
+//     >
+//       {title}
+//     </button>
+//   );
+// };
+
+// export default Card;
+
+import React from "react";
 
 interface CardProps {
   title: string;
   onClick: () => void;
+  className?: string;
 }
 
-const Card: FC<CardProps> = ({ title, onClick }) => {
+const Card: React.FC<CardProps> = ({ title, onClick, className }) => {
   return (
-    <button
+    <div
+      className={`p-4 border rounded shadow hover:shadow-md transition-shadow cursor-pointer ${className}`}
       onClick={onClick}
-      className="p-4 bg-white border rounded-lg shadow-md hover:bg-gray-100 transition-all"
     >
-      {title}
-    </button>
+      <h3 className="text-lg">{title}</h3>
+    </div>
   );
 };
 
