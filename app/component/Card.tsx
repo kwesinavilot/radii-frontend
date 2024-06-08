6d507e3cdec38a7ed53fc9981d6e925501850674
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-06-08 23:13:28 +0000

    Work on the border color

--- app/component/Card.tsx
+++ app/component/Card.tsx
@@ -9,10 +9,11 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ title, onClick, className }) => {
   return (
     <div
-      className={`p-4 border-2 rounded shadow hover:shadow-md transition-shadow cursor-pointer ${className}`}
+      className={`p-4 border rounded-xl shadow hover:shadow-md transition-shadow cursor-pointer ${className}`}
       onClick={onClick}
       style={{
-        borderImage: "linear-gradient(to right, #ff7e5f, #feb47b) 1",
+        borderImage:
+          "linear-gradient(to right,  #3788E5 0%, #3788E5 20%, #E58A13 20%, #E58A13 40%, #3788E5 40%, #3788E5 60%, #3788E5 60%, #3788E5 80%, #E58A13 80%, #E58A13 100%) 1",
       }}
     >
       <h3 className="text-lg">{title}</h3>
