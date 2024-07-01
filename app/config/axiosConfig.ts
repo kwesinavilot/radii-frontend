51d3ebf5ed3fe3d8aa191d823782e7d34518e963
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-07-01 17:11:44 +0000

    update the receiving of docs

--- app/config/axiosConfig.ts
+++ app/config/axiosConfig.ts
@@ -1,11 +1,26 @@
+// import Cookies from "js-cookie";
+
+// const generateAxiosConfig = () => {
+//   const userToken = Cookies.get("auth_token");
+//   return {
+//     headers: {
+//       "Content-Type": "application/json",
+//       ...(userToken ? { Authorization: `Token ${userToken}` } : {}),
+//     },
+//   };
+// };
+
+// export default generateAxiosConfig;
+
 import Cookies from "js-cookie";
 
-const generateAxiosConfig = () => {
+const generateAxiosConfig = (orgID: string) => {
   const userToken = Cookies.get("auth_token");
   return {
     headers: {
       "Content-Type": "application/json",
       ...(userToken ? { Authorization: `Token ${userToken}` } : {}),
+      ...(orgID ? { orgID } : {}),
     },
   };
 };
