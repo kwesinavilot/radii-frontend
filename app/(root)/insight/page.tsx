a4f2ebe6cb3018969b5c11009a7c5d19fdf2b83c
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-07-02 00:24:55 +0000

    update the result

--- app/(root)/insight/page.tsx
+++ app/(root)/insight/page.tsx
@@ -1,3 +1,139 @@
+// // "use client";
+
+// // import Card from "@/app/component/Card";
+// // import Loader from "@/app/component/Loader";
+// // import Navbar from "@/app/component/NavBar";
+// // import SearchBar from "@/app/component/SearchBar";
+// // import SearchResult from "@/app/component/SearchResult";
+// // import Image from "next/image";
+// // import { useState } from "react";
+// // import axios from "axios";
+// // import generateAxiosConfig from "@/app/config/axiosConfig";
+// // import { RootState } from "@/app/store/store";
+// // import { useSelector } from "react-redux";
+// // import { toast } from "react-toastify";
+
+// // interface Result {
+// //   type: string;
+// //   title: string;
+// //   status: string;
+// //   dateAdded: string;
+// //   lastUpdated: string;
+// // }
+
+// // const questions: string[] = [
+// //   "I want to see insight from Q1 and Q2",
+// //   "I want to see the trends on my order",
+// //   "Give me some insight about the highest location movement",
+// //   "Give me some insight about Order Volume and Trends",
+// // ];
+
+// // const dummyResult: Result = {
+// //   type: "PDF",
+// //   title: "Total Customer Movement in Quarter 1 of 2023",
+// //   status: "Complete",
+// //   dateAdded: "05/03/2024",
+// //   lastUpdated: "05/03/2024",
+// // };
+
+// // export default function Insight() {
+// //   const [inputValue, setInputValue] = useState("");
+// //   const [isLoading, setIsLoading] = useState(false);
+// //   const [selectedFolder, setSelectedFolder] = useState("Movement DS");
+// //   const [remainingPrompts, setRemainingPrompts] = useState(10);
+// //   const [showResult, setShowResult] = useState(false);
+// //   const [result, setResult] = useState<Result | null>(null);
+
+// //   const token = useSelector((state: RootState) => state.auth.token);
+// //   const orgID = useSelector((state: RootState) => state.auth.orgID);
+
+// //   const handleCardClick = (question: string) => {
+// //     setInputValue(question);
+// //   };
+
+// //   const handleSearchSubmit = async () => {
+// //     if (inputValue.trim()) {
+// //       setIsLoading(true);
+// //       setRemainingPrompts((prev) => (prev > 0 ? prev - 1 : 0));
+// //       try {
+// //         const response = await axios.post(
+// //           "https://lobster-app-9ufhi.ondigitalocean.app/insights/internal/",
+// //           {
+// //             query: inputValue,
+// //             orgID: orgID,
+// //             sourceType: "folder",
+// //             folderID: "static",
+// //           },
+// //           generateAxiosConfig()
+// //         );
+// //         setResult(response.data);
+// //         console.log("Insights fetched successfully:", response.data);
+// //         setShowResult(true);
+// //       } catch (error) {
+// //         console.error("Error fetching insights:", error);
+// //         toast.error("Error fetching insights");
+// //       } finally {
+// //         setIsLoading(false);
+// //       }
+// //     }
+// //   };
+
+// //   return (
+// //     <div className="bg-grey-bg h-screen overflow-y-auto">
+// //       <Navbar title="Dashboard" icon="" />
+// //       <div className="grid grid-cols-1 sm:grid-cols-4 p-2 py-4 h-full overflow-y-auto">
+// //         <div className="block sm:col-span-1 py-4 px-8 bg-white border border-gray-200 rounded-lg shadow dark:border-gray-100">
+// //           <h2 className="text-xl font-bold mb-4">Library</h2>
+// //           <p>Nothing here yet</p>
+// //         </div>
+// //         <main className="sm:col-span-3 block py-4 px-8 bg-white border border-gray-200 rounded-lg shadow dark:border-gray-100 mainInternal">
+// //           <div className="w-5/6 mx-auto mt-4">
+// //             <h1 className="text-[40px] text-center font-bold mb-4">
+// //               Ask Radii A Question
+// //             </h1>
+// //             <div className="grid grid-cols-2 gap-4 mb-8">
+// //               {questions.map((question, index) => (
+// //                 <Card
+// //                   key={index}
+// //                   title={question}
+// //                   onClick={() => handleCardClick(question)}
+// //                   className="h-20 w-full"
+// //                 />
+// //               ))}
+// //             </div>
+// //           </div>
+// //           <div className="relative">
+// //             <SearchBar
+// //               value={inputValue}
+// //               onChange={setInputValue}
+// //               onSubmit={handleSearchSubmit}
+// //               selectedFolder={selectedFolder}
+// //               onFolderChange={setSelectedFolder}
+// //               remainingPrompts={remainingPrompts}
+// //             />
+// //             {isLoading && (
+// //               <div className="flex mt-4">
+// //                 <div className="flex items-center gap-2">
+// //                   <Image
+// //                     src="/i-logo.svg"
+// //                     alt="Radiis logo"
+// //                     width={22}
+// //                     height={22}
+// //                   />
+// //                   <Loader />
+// //                 </div>
+// //               </div>
+// //             )}
+// //           </div>
+// //           {!isLoading && showResult && result && (
+// //             <SearchResult result={result} />
+// //           )}
+// //         </main>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
 // "use client";
 
 // import Card from "@/app/component/Card";
@@ -7,6 +143,11 @@
 // import SearchResult from "@/app/component/SearchResult";
 // import Image from "next/image";
 // import { useState } from "react";
+// import axios from "axios";
+// import generateAxiosConfig from "@/app/config/axiosConfig";
+// import { RootState } from "@/app/store/store";
+// import { useSelector } from "react-redux";
+// import { toast } from "react-toastify";
 
 // interface Result {
 //   type: string;
@@ -37,19 +178,41 @@
 //   const [selectedFolder, setSelectedFolder] = useState("Movement DS");
 //   const [remainingPrompts, setRemainingPrompts] = useState(10);
 //   const [showResult, setShowResult] = useState(false);
+//   const [result, setResult] = useState<Result | null>(null);
+
+//   const token = useSelector((state: RootState) => state.auth.token);
+//   const orgID = useSelector((state: RootState) => state.auth.orgID);
 
 //   const handleCardClick = (question: string) => {
 //     setInputValue(question);
 //   };
 
-//   const handleSearchSubmit = () => {
+//   const handleSearchSubmit = async () => {
 //     if (inputValue.trim()) {
 //       setIsLoading(true);
 //       setRemainingPrompts((prev) => (prev > 0 ? prev - 1 : 0));
-//       setTimeout(() => {
-//         setIsLoading(false);
+//       setInputValue("");
+
+//       try {
+//         const response = await axios.post(
+//           "https://lobster-app-9ufhi.ondigitalocean.app/insights/internal/",
+//           {
+//             query: inputValue,
+//             orgID: orgID,
+//             sourceType: "folder",
+//             folderID: "static",
+//           },
+//           generateAxiosConfig()
+//         );
+//         setResult(response.data);
+//         console.log("Insights fetched successfully:", response.data);
 //         setShowResult(true);
-//       }, 2000);
+//       } catch (error) {
+//         console.error("Error fetching insights:", error);
+//         toast.error("Error fetching insights");
+//       } finally {
+//         setIsLoading(false);
+//       }
 //     }
 //   };
 
@@ -90,7 +253,7 @@
 //               <div className="flex mt-4">
 //                 <div className="flex items-center gap-2">
 //                   <Image
-//                     src="/u-logo.png"
+//                     src="/i-logo.svg"
 //                     alt="Radiis logo"
 //                     width={22}
 //                     height={22}
@@ -100,7 +263,9 @@
 //               </div>
 //             )}
 //           </div>
-//           {!isLoading && showResult && <SearchResult result={dummyResult} />}
+//           {!isLoading && showResult && result && (
+//             <SearchResult result={result} />
+//           )}
 //         </main>
 //       </div>
 //     </div>
@@ -128,6 +293,7 @@ interface Result {
   status: string;
   dateAdded: string;
   lastUpdated: string;
+  data: any;
 }
 
 const questions: string[] = [
@@ -136,14 +302,6 @@ const questions: string[] = [
   "Give me some insight about the highest location movement",
   "Give me some insight about Order Volume and Trends",
 ];
-
-const dummyResult: Result = {
-  type: "PDF",
-  title: "Total Customer Movement in Quarter 1 of 2023",
-  status: "Complete",
-  dateAdded: "05/03/2024",
-  lastUpdated: "05/03/2024",
-};
 
 export default function Insight() {
   const [inputValue, setInputValue] = useState("");
@@ -164,6 +322,8 @@ export default function Insight() {
     if (inputValue.trim()) {
       setIsLoading(true);
       setRemainingPrompts((prev) => (prev > 0 ? prev - 1 : 0));
+      setInputValue("");
+
       try {
         const response = await axios.post(
           "https://lobster-app-9ufhi.ondigitalocean.app/insights/internal/",
--- app/component/SearchResult.tsx
+++ app/component/SearchResult.tsx
@@ -5,8 +5,19 @@
 //   IoCopyOutline,
 //   IoDownloadOutline,
 //   IoShareSocialOutline,
+//   IoSaveOutline,
+//   IoEyeOutline,
+//   IoBarChartOutline,
+//   IoListOutline,
+//   IoDocumentTextOutline,
 // } from "react-icons/io5";
-// import { FaRegThumbsDown, FaRegThumbsUp } from "react-icons/fa";
+// import {
+//   FaRegThumbsDown,
+//   FaRegThumbsUp,
+//   FaPlus,
+//   FaChartLine,
+// } from "react-icons/fa";
+// import Select, { SingleValue } from "react-select";
 
 // interface Result {
 //   type: string;
@@ -20,8 +31,14 @@
 //   result: Result;
 // }
 
+// interface CustomOption {
+//   value: string;
+//   label: string;
+//   icon: React.ReactNode;
+// }
+
 // const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
-//   const [viewType, setViewType] = useState("Bar");
+//   const [viewType, setViewType] = useState("Table");
 
 //   const data = {
 //     labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
@@ -42,6 +59,61 @@
 //     },
 //   };
 
+//   const tableData = [
+//     {
+//       productName: "Grace Onyinyee",
+//       location: "Accra Ghana",
+//       sales: 423,
+//       amount: "$34,295",
+//       imgUrl: "/bright.png",
+//     },
+//     {
+//       productName: "Oyinlade Ojesanmi",
+//       location: "Accra Ghana",
+//       sales: 423,
+//       amount: "$34,295",
+//       imgUrl: "/bright.png",
+//     },
+//     {
+//       productName: "Kwesi Navilot",
+//       location: "Accra Ghana",
+//       sales: 423,
+//       amount: "$34,295",
+//       imgUrl: "/bright.png",
+//     },
+//   ];
+
+//   const customOptions: CustomOption[] = [
+//     { value: "Bar", label: "Bar Charts", icon: <IoBarChartOutline /> },
+//     { value: "Line", label: "Line Charts", icon: <FaChartLine /> },
+//     { value: "Text", label: "Text View", icon: <IoDocumentTextOutline /> },
+//     { value: "Table", label: "Table View", icon: <IoListOutline /> },
+//   ];
+
+//   const customStyles = {
+//     control: (provided: any) => ({
+//       ...provided,
+//       display: "flex",
+//     }),
+//     singleValue: (provided: any) => ({
+//       ...provided,
+//       display: "flex",
+//       alignItems: "center",
+//     }),
+//     option: (provided: any) => ({
+//       ...provided,
+//       display: "flex",
+//       alignItems: "center",
+//     }),
+//   };
+
+//   const renderOption = (option: CustomOption) => (
+//     <div className="flex items-center">
+//       {option.icon}
+//       <span className="ml-2">{option.label}</span>
+//     </div>
+//   );
+
 //   const renderChart = () => {
 //     switch (viewType) {
 //       case "Bar":
@@ -50,7 +122,7 @@
 //         return <Line data={data} options={options} />;
 //       case "Text":
 //         return (
-//           <div className="text-lg">
+//           <div className="text-lg mt-4">
 //             <p>January: 10</p>
 //             <p>February: 20</p>
 //             <p>March: 30</p>
@@ -58,6 +130,36 @@
 //             <p>May: 25</p>
 //             <p>June: 35</p>
 //           </div>
+//         );
+//       case "Table":
+//         return (
+//           <table className="min-w-full bg-white">
+//             <thead>
+//               <tr>
+//                 <th className="py-2 px-4 border-b">Product Name</th>
+//                 <th className="py-2 px-4 border-b">Location</th>
+//                 <th className="py-2 px-4 border-b">Sales</th>
+//                 <th className="py-2 px-4 border-b">Amount</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {tableData.map((data, index) => (
+//                 <tr key={index}>
+//                   <td className="py-2 px-4 border-b flex items-center">
+//                     <img
+//                       src={data.imgUrl}
+//                       alt={data.productName}
+//                       className="w-10 h-10 rounded-full mr-2"
+//                     />
+//                     {data.productName}
+//                   </td>
+//                   <td className="py-2 px-4 border-b">{data.location}</td>
+//                   <td className="py-2 px-4 border-b">{data.sales}</td>
+//                   <td className="py-2 px-4 border-b">{data.amount}</td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
 //         );
 //       default:
 //         return null;
@@ -68,30 +170,34 @@
 //     <div className="mt-4 p-4 bg-white border border-gray-200 rounded-lg shadow">
 //       <h2 className="text-2xl font-bold mb-4">{result.title}</h2>
 //       <div className="flex justify-between items-center mb-4">
-//         <div>
-//           <select
-//             className="px-4 py-2 bg-gray-200 rounded shadow mr-2"
-//             value={viewType}
-//             onChange={(e) => setViewType(e.target.value)}
-//           >
-//             <option value="Bar">Bar Charts</option>
-//             <option value="Line">Line Charts</option>
-//             <option value="Text">Text View</option>
-//           </select>
+//         <div className="relative">
+//           <Select
+//             value={customOptions.find((option) => option.value === viewType)}
+//             // onChange={(selectedOption) => setViewType(selectedOption.value)}
+//             onChange={(selectedOption) =>
+//               selectedOption && setViewType(selectedOption.value)
+//             }
+//             options={customOptions}
+//             styles={customStyles}
+//             formatOptionLabel={renderOption}
+//           />
 //         </div>
-//         <div className="">
-//           <button className="px-4 py-2 bg-gray-200 rounded shadow mr-2">
+//         <div className="flex space-x-2">
+//           <button className="flex items-center px-4 py-2 bg-gray-200 rounded shadow">
+//             <IoSaveOutline className="mr-1" />
 //             Save
 //           </button>
-//           <button className="px-4 py-2 bg-gray-200 rounded shadow mr-2">
+//           <button className="flex items-center px-4 py-2 bg-gray-200 rounded shadow">
+//             <FaPlus className="mr-1" />
 //             Add to Views
 //           </button>
-//           <button className="px-4 py-2 bg-gray-200 rounded shadow">
+//           <button className="flex items-center px-4 py-2 bg-gray-200 rounded shadow">
+//             <IoEyeOutline className="mr-1" />
 //             View Reference
 //           </button>
 //         </div>
 //       </div>
-//       <div className="w-4/6 border border-gray-200 rounded-lg shadow">
+//       <div className="w-full sm:w-4/6 border border-gray-200 rounded-lg shadow">
 //         <div className="flex justify-between items-center mb-4">
 //           <div style={{ width: "100%" }}>{renderChart()}</div>
 //         </div>
@@ -169,6 +275,7 @@ interface Result {
   status: string;
   dateAdded: string;
   lastUpdated: string;
+  data: any;
 }
 
 interface SearchResultProps {
@@ -185,11 +292,11 @@ const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
   const [viewType, setViewType] = useState("Table");
 
   const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: result.data.labels,
     datasets: [
       {
-        label: "Customer Movement",
-        data: [10, 20, 30, 40, 25, 35],
+        label: result.title,
+        data: result.data.values,
         backgroundColor: "#038C7F",
       },
     ],
@@ -202,30 +309,6 @@ const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
       },
     },
   };
-
-  const tableData = [
-    {
-      productName: "Grace Onyinyee",
-      location: "Accra Ghana",
-      sales: 423,
-      amount: "$34,295",
-      imgUrl: "/bright.png",
-    },
-    {
-      productName: "Oyinlade Ojesanmi",
-      location: "Accra Ghana",
-      sales: 423,
-      amount: "$34,295",
-      imgUrl: "/bright.png",
-    },
-    {
-      productName: "Kwesi Navilot",
-      location: "Accra Ghana",
-      sales: 423,
-      amount: "$34,295",
-      imgUrl: "/bright.png",
-    },
-  ];
 
   const customOptions: CustomOption[] = [
     { value: "Bar", label: "Bar Charts", icon: <IoBarChartOutline /> },
@@ -267,12 +350,9 @@ const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
       case "Text":
         return (
           <div className="text-lg mt-4">
-            <p>January: 10</p>
-            <p>February: 20</p>
-            <p>March: 30</p>
-            <p>April: 40</p>
-            <p>May: 25</p>
-            <p>June: 35</p>
+            {result.data.values.map((value: number, index: number) => (
+              <p key={index}>{`${result.data.labels[index]}: ${value}`}</p>
+            ))}
           </div>
         );
       case "Table":
@@ -280,26 +360,17 @@ const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
           <table className="min-w-full bg-white">
             <thead>
               <tr>
-                <th className="py-2 px-4 border-b">Product Name</th>
-                <th className="py-2 px-4 border-b">Location</th>
-                <th className="py-2 px-4 border-b">Sales</th>
-                <th className="py-2 px-4 border-b">Amount</th>
+                <th className="py-2 px-4 border-b">Label</th>
+                <th className="py-2 px-4 border-b">Value</th>
               </tr>
             </thead>
             <tbody>
-              {tableData.map((data, index) => (
+              {result.data.labels.map((label: string, index: number) => (
                 <tr key={index}>
-                  <td className="py-2 px-4 border-b flex items-center">
-                    <img
-                      src={data.imgUrl}
-                      alt={data.productName}
-                      className="w-10 h-10 rounded-full mr-2"
-                    />
-                    {data.productName}
+                  <td className="py-2 px-4 border-b">{label}</td>
+                  <td className="py-2 px-4 border-b">
+                    {result.data.values[index]}
                   </td>
-                  <td className="py-2 px-4 border-b">{data.location}</td>
-                  <td className="py-2 px-4 border-b">{data.sales}</td>
-                  <td className="py-2 px-4 border-b">{data.amount}</td>
                 </tr>
               ))}
             </tbody>
@@ -317,7 +388,6 @@ const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
         <div className="relative">
           <Select
             value={customOptions.find((option) => option.value === viewType)}
-            // onChange={(selectedOption) => setViewType(selectedOption.value)}
             onChange={(selectedOption) =>
               selectedOption && setViewType(selectedOption.value)
             }
@@ -365,8 +435,8 @@ const SearchResult: React.FC<SearchResultProps> = ({ result }) => {
       </div>
       <div className="flex items-center justify-between mt-6">
         <div className="flex gap-4">
-          <span>07/06/2024</span>
-          <span>08:27:54am</span>
+          <span>{result.dateAdded}</span>
+          <span>{new Date(result.dateAdded).toLocaleTimeString()}</span>
         </div>
         <div className="flex gap-2">
           <p className="border border-gray-200 rounded-sm px-2 py-1">
