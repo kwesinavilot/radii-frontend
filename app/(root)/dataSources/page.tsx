b582e42c1cd4ed6bd2296991fb0cecc78cd3b193
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-07-09 00:02:44 +0000

    The drive connection is stable

--- app/(root)/dataSources/page.tsx
+++ app/(root)/dataSources/page.tsx
@@ -126,8 +126,215 @@
 
 // // export default DataSourceTable;
 
-"use client";
+// "use client";
 
+// import Navbar from "@/app/component/NavBar";
+// import { RootState } from "@/app/store/store";
+// import Link from "next/link";
+// import React, { useEffect, useState } from "react";
+// import { FaRegTrashAlt } from "react-icons/fa";
+// import { useSelector } from "react-redux";
+// import axios from "axios";
+// import generateAxiosConfig from "@/app/config/axiosConfig";
+
+// import "react-toastify/dist/ReactToastify.css";
+// import { toast } from "react-toastify";
+
+// interface FileItem {
+//   sourceID: string;
+//   type: string;
+//   name: string;
+//   source: string;
+//   description: string | null;
+//   created_at: string;
+// }
+
+// const DataSourceTable: React.FC = () => {
+//   const [activeTab, setActiveTab] = useState("files");
+//   const [dataSources, setDataSources] = useState([]);
+//   const token = useSelector((state: RootState) => state.auth.token);
+//   const orgID = useSelector((state: RootState) => state.auth.orgID);
+//   const [files, setFiles] = useState<FileItem[]>([]);
+//   const [drive, setDrive] = useState<FileItem[]>([]);
+//   const [database, setDatabase] = useState<FileItem[]>([]);
+//   const [loading, setLoading] = useState(false);
+
+//   useEffect(() => {
+//     if (token && orgID) {
+//       fetchDataSources();
+//       fetchDriveData();
+//       // fetchDatabaseData();
+//     }
+//   }, [token, orgID]);
+
+//   console.log(orgID);
+
+//   const fetchDataSources = async () => {
+//     try {
+//       if (!orgID) {
+//         throw new Error("orgID is required");
+//       }
+//       const response = await axios.get(
+//         `https://lobster-app-9ufhi.ondigitalocean.app/datasources/static/`,
+//         {
+//           ...generateAxiosConfig(),
+//           params: {
+//             orgID: orgID,
+//           },
+//         }
+//       );
+
+//       setFiles(response.data);
+//       console.log("Files fetched successfully:", response.data);
+//     } catch (error) {
+//       console.error("Error fetching files:", error);
+//       toast.error("Error fetching files");
+//     }
+//   };
+
+//   const fetchDriveData = async () => {
+//     try {
+//       if (!orgID) {
+//         throw new Error("orgID is required");
+//       }
+//       const response = await axios.get(
+//         `https://lobster-app-9ufhi.ondigitalocean.app/datasources/gdrive/`,
+//         {
+//           ...generateAxiosConfig(),
+//           params: {
+//             orgID: orgID,
+//           },
+//         }
+//       );
+//       console.log(response);
+//       console.log("Drive data fetched successfully:", response.data);
+//       setDrive(response.data);
+//     } catch (error) {
+//       console.error("Error fetching drive data:", error);
+//       toast.error("Error fetching drive data");
+//     }
+//   };
+
+//   // const fetchDatabaseData = async () => {
+//   //   try {
+//   //     if (!orgID) {
+//   //       throw new Error("orgID is required");
+//   //     }
+//   //     const response = await axios.get(
+//   //       `https://backend.getradii.com/datasources/database/${orgID}/`,
+//   //       {
+//   //         ...generateAxiosConfig(),
+//   //       }
+//   //     );
+
+//   //     setDatabase(response.data);
+//   //     // console.log("Database data fetched successfully:", response.data);
+//   //   } catch (error) {
+//   //     // console.error("Error fetching database data:", error);
+//   //     // toast.error("Error fetching database data");
+//   //   }
+//   // };
+
+//   const renderTable = (data: FileItem[]) => (
+//     <div className="overflow-x-auto">
+//       <table className="min-w-full table-auto">
+//         <thead>
+//           <tr className="bg-[#1D1D1D] text-white">
+//             <th className="px-4 py-2 text-left">Type</th>
+//             <th className="px-4 py-2 text-left">Title</th>
+//             <th className="px-4 py-2 text-left">Date Added</th>
+//             <th className="px-4 py-2 text-left">Actions</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {loading ? (
+//             <tr>
+//               <td colSpan={4} className="px-4 py-2 text-center text-gray-500">
+//                 Loading...
+//               </td>
+//             </tr>
+//           ) : data.length > 0 ? (
+//             data.map((item) => (
+//               <tr key={item.sourceID} className="border-t">
+//                 <td className="px-4 py-2">{item.type}</td>
+//                 <td className="px-4 py-2">{item.name}</td>
+//                 <td className="px-4 py-2">{item.created_at}</td>
+//                 <td className="px-4 py-2">
+//                   <button className="text-red-600 hover:text-red-800">
+//                     <FaRegTrashAlt />
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))
+//           ) : (
+//             <tr>
+//               <td colSpan={4} className="px-4 py-2 text-center text-gray-500">
+//                 No items found.
+//               </td>
+//             </tr>
+//           )}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+
+//   return (
+//     <div className="bg-grey-bg h-screen overflow-y-auto">
+//       <Navbar title="Data Source" icon="" />
+//       <div className="bg-white h-screen px-6 py-8 m-4 overflow-hidden rounded shadow">
+//         <div className="flex justify-between items-center mb-4">
+//           <h2 className="text-xl font-semibold">Data Sources</h2>
+//           <button className="px-4 py-2 text-white bg-orange-500 rounded hover:bg-orange-600 flex items-center gap-2">
+//             <Link href="/connectDataSources">Add Source</Link>
+//           </button>
+//         </div>
+
+//         <div className="flex mb-4">
+//           <button
+//             className={`px-4 py-2 rounded-t-lg ${
+//               activeTab === "files"
+//                 ? "bg-orange-500 text-white"
+//                 : "bg-gray-200 text-gray-600"
+//             }`}
+//             onClick={() => setActiveTab("files")}
+//           >
+//             Files
+//           </button>
+//           <button
+//             className={`px-4 py-2 rounded-t-lg ml-2 ${
+//               activeTab === "drive"
+//                 ? "bg-orange-500 text-white"
+//                 : "bg-gray-200 text-gray-600"
+//             }`}
+//             onClick={() => setActiveTab("drive")}
+//           >
+//             Drive
+//           </button>
+//           <button
+//             className={`px-4 py-2 rounded-t-lg ml-2 ${
+//               activeTab === "database"
+//                 ? "bg-orange-500 text-white"
+//                 : "bg-gray-200 text-gray-600"
+//             }`}
+//             onClick={() => setActiveTab("database")}
+//           >
+//             Database
+//           </button>
+//         </div>
+
+//         <div>
+//           {activeTab === "files" && renderTable(files)}
+//           {activeTab === "drive" && renderTable(drive)}
+//           {activeTab === "database" && renderTable(database)}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default DataSourceTable;
+
+"use client";
 import Navbar from "@/app/component/NavBar";
 import { RootState } from "@/app/store/store";
 import Link from "next/link";
@@ -136,6 +343,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import generateAxiosConfig from "@/app/config/axiosConfig";
+import { format } from "date-fns";
 
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
@@ -149,32 +357,34 @@ interface FileItem {
   created_at: string;
 }
 
+interface DriveItem {
+  integrationID: string;
+  service: string;
+  username: string;
+  created_at: string;
+}
+
 const DataSourceTable: React.FC = () => {
   const [activeTab, setActiveTab] = useState("files");
-  const [dataSources, setDataSources] = useState([]);
+  const [files, setFiles] = useState<FileItem[]>([]);
+  const [drive, setDrive] = useState<DriveItem[]>([]);
+  const [loading, setLoading] = useState(false);
   const token = useSelector((state: RootState) => state.auth.token);
   const orgID = useSelector((state: RootState) => state.auth.orgID);
-  const [files, setFiles] = useState<FileItem[]>([]);
-  const [drive, setDrive] = useState<FileItem[]>([]);
-  const [database, setDatabase] = useState<FileItem[]>([]);
-  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (token && orgID) {
       fetchDataSources();
       fetchDriveData();
-      fetchDatabaseData();
     }
   }, [token, orgID]);
-
-  console.log(orgID);
 
   const fetchDataSources = async () => {
     try {
       if (!orgID) {
         throw new Error("orgID is required");
       }
-      const response = await axios.get(
+      const response = await axios.get<FileItem[]>(
         `https://lobster-app-9ufhi.ondigitalocean.app/datasources/static/`,
         {
           ...generateAxiosConfig(),
@@ -197,8 +407,8 @@ const DataSourceTable: React.FC = () => {
       if (!orgID) {
         throw new Error("orgID is required");
       }
-      const response = await axios.get(
-        `https://backend.getradii.com/datasources/gdrive/`,
+      const response = await axios.get<DriveItem[]>(
+        `https://lobster-app-9ufhi.ondigitalocean.app/datasources/gdrive/`,
         {
           ...generateAxiosConfig(),
           params: {
@@ -215,27 +425,37 @@ const DataSourceTable: React.FC = () => {
     }
   };
 
-  const fetchDatabaseData = async () => {
+  const handleDeleteFile = async (id: string) => {
     try {
-      if (!orgID) {
-        throw new Error("orgID is required");
-      }
-      const response = await axios.get(
-        `https://backend.getradii.com/datasources/database/${orgID}/`,
-        {
-          ...generateAxiosConfig(),
-        }
+      const response = await axios.delete(
+        `https://lobster-app-9ufhi.ondigitalocean.app/datasources/static/${id}`,
+        generateAxiosConfig()
       );
-
-      setDatabase(response.data);
-      console.log("Database data fetched successfully:", response.data);
+      console.log("File deleted successfully:", response.data);
+      toast.success("File deleted successfully");
+      fetchDataSources();
     } catch (error) {
-      console.error("Error fetching database data:", error);
-      toast.error("Error fetching database data");
+      console.error("Error deleting file:", error);
+      toast.error("Error deleting file");
     }
   };
 
-  const renderTable = (data: FileItem[]) => (
+  const handleDeleteDrive = async (id: string) => {
+    try {
+      const response = await axios.delete(
+        `https://lobster-app-9ufhi.ondigitalocean.app/datasources/gdrive/${id}`,
+        generateAxiosConfig()
+      );
+      console.log("Drive integration deleted successfully:", response.data);
+      toast.success("Drive integration deleted successfully");
+      fetchDriveData();
+    } catch (error) {
+      console.error("Error deleting drive integration:", error);
+      toast.error("Error deleting drive integration");
+    }
+  };
+
+  const renderFilesTable = (data: FileItem[]) => (
     <div className="overflow-x-auto">
       <table className="min-w-full table-auto">
         <thead>
@@ -258,9 +478,14 @@ const DataSourceTable: React.FC = () => {
               <tr key={item.sourceID} className="border-t">
                 <td className="px-4 py-2">{item.type}</td>
                 <td className="px-4 py-2">{item.name}</td>
-                <td className="px-4 py-2">{item.created_at}</td>
                 <td className="px-4 py-2">
-                  <button className="text-red-600 hover:text-red-800">
+                  {format(new Date(item.created_at), "yyyy-MM-dd")}
+                </td>
+                <td className="px-4 py-2">
+                  <button
+                    className="text-red-600 hover:text-red-800"
+                    onClick={() => handleDeleteFile(item.sourceID)}
+                  >
                     <FaRegTrashAlt />
                   </button>
                 </td>
@@ -269,7 +494,55 @@ const DataSourceTable: React.FC = () => {
           ) : (
             <tr>
               <td colSpan={4} className="px-4 py-2 text-center text-gray-500">
-                No items found.
+                No files found.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+
+  const renderDriveTable = (data: DriveItem[]) => (
+    <div className="overflow-x-auto">
+      <table className="min-w-full table-auto">
+        <thead>
+          <tr className="bg-[#1D1D1D] text-white">
+            <th className="px-4 py-2 text-left">Service</th>
+            <th className="px-4 py-2 text-left">Username</th>
+            <th className="px-4 py-2 text-left">Date Connected</th>
+            <th className="px-4 py-2 text-left">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
+            <tr>
+              <td colSpan={4} className="px-4 py-2 text-center text-gray-500">
+                Loading...
+              </td>
+            </tr>
+          ) : data.length > 0 ? (
+            data.map((item) => (
+              <tr key={item.integrationID} className="border-t">
+                <td className="px-4 py-2">{item.service}</td>
+                <td className="px-4 py-2">{item.username}</td>
+                <td className="px-4 py-2">
+                  {format(new Date(item.created_at), "yyyy-MM-dd")}
+                </td>
+                <td className="px-4 py-2">
+                  <button
+                    className="text-red-600 hover:text-red-800"
+                    onClick={() => handleDeleteDrive(item.integrationID)}
+                  >
+                    <FaRegTrashAlt />
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={4} className="px-4 py-2 text-center text-gray-500">
+                No drive integrations found.
               </td>
             </tr>
           )}
@@ -310,22 +583,11 @@ const DataSourceTable: React.FC = () => {
           >
             Drive
           </button>
-          <button
-            className={`px-4 py-2 rounded-t-lg ml-2 ${
-              activeTab === "database"
-                ? "bg-orange-500 text-white"
-                : "bg-gray-200 text-gray-600"
-            }`}
-            onClick={() => setActiveTab("database")}
-          >
-            Database
-          </button>
         </div>
 
         <div>
-          {activeTab === "files" && renderTable(files)}
-          {activeTab === "drive" && renderTable(drive)}
-          {activeTab === "database" && renderTable(database)}
+          {activeTab === "files" && renderFilesTable(files)}
+          {activeTab === "drive" && renderDriveTable(drive)}
         </div>
       </div>
     </div>
