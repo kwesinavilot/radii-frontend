85a42aee12729331265e4682110764a3f19a9416
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-07-10 11:49:39 +0000

    Update the auth

--- app/store/authSlice.tsx
+++ app/store/authSlice.tsx
@@ -1,7 +1,44 @@
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { exists } from "fs";
+// import Cookies from "js-cookie";
+// import { pages } from "next/dist/build/templates/app-page";
+
+// interface AuthState {
+//   token: string | null;
+//   orgID: string | null;
+// }
+
+// const initialState: AuthState = {
+//   token: Cookies.get("auth_token") || null,
+//   orgID: null,
+// };
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState,
+//   reducers: {
+//     setToken: (state, action: PayloadAction<string>) => {
+//       state.token = action.payload;
+//       Cookies.set("auth_token", action.payload);
+//     },
+//     clearToken: (state) => {
+//       state.token = null;
+//       Cookies.remove("auth_token");
+//     },
+//     setOrgID: (state, action: PayloadAction<string>) => {
+//       state.orgID = action.payload;
+//     },
+//     clearOrgID: (state) => {
+//       state.orgID = null;
+//     },
+//   },
+// });
+
+// export const { setToken, clearToken, setOrgID, clearOrgID } = authSlice.actions;
+// export default authSlice.reducer;
+
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { exists } from "fs";
 import Cookies from "js-cookie";
-import { pages } from "next/dist/build/templates/app-page";
 
 interface AuthState {
   token: string | null;
@@ -20,16 +57,20 @@ const authSlice = createSlice({
     setToken: (state, action: PayloadAction<string>) => {
       state.token = action.payload;
       Cookies.set("auth_token", action.payload);
+      console.log("Token set:", action.payload);
     },
     clearToken: (state) => {
       state.token = null;
       Cookies.remove("auth_token");
+      console.log("Token cleared");
     },
     setOrgID: (state, action: PayloadAction<string>) => {
       state.orgID = action.payload;
+      console.log("OrgID set:", action.payload);
     },
     clearOrgID: (state) => {
       state.orgID = null;
+      console.log("OrgID cleared");
     },
   },
 });
