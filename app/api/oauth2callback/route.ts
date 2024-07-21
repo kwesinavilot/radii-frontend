5cadaed5086fee3f11c3552698ee996c37282a06
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-07-21 22:27:03 +0000

    Trying to make drive works

--- app/api/oauth2callback/route.ts
+++ app/api/oauth2callback/route.ts
@@ -1,3 +1,136 @@
+// // import { NextRequest, NextResponse } from "next/server";
+// // import axios from "axios";
+// // import Cookies from "js-cookie";
+
+// // export async function GET(req: NextRequest) {
+// //   console.log("OAuth2 callback handler:");
+
+// //   const { searchParams } = new URL(req.url);
+// //   const state = searchParams.get("state");
+// //   const code = searchParams.get("code");
+// //   const scopes = searchParams.get("scopes");
+// //   const authuser = searchParams.get("authuser");
+// //   const prompt = searchParams.get("prompt");
+
+// //   console.log("Received query params:", state, code, scopes, authuser, prompt);
+
+// //   if (!code) {
+// //     return NextResponse.json(
+// //       { error: "Authorization code is missing" },
+// //       { status: 400 }
+// //     );
+// //   }
+
+// //   const token = req.cookies.get("auth_token");
+// //   console.log(token);
+// //   if (!token) {
+// //     return NextResponse.json(
+// //       { error: "User token is missing" },
+// //       { status: 401 }
+// //     );
+// //   }
+
+// //   try {
+// //     const response = await axios.post(
+// //       "https://starfish-app-9ezx5.ondigitalocean.app/datasources/gdrive/",
+// //       {
+// //         code,
+// //       },
+// //       {
+// //         headers: {
+// // Authorization: `Token ${token.value}`,
+// //         },
+// //       }
+// //     );
+
+// //     const data = response.data;
+// //     console.log("Response from backend:", data);
+
+// //     if (response.status === 200) {
+// //       const { email } = response.data;
+
+// //       return NextResponse.redirect(
+// //         `/dataSources?email=${encodeURIComponent(email)}`
+// //       );
+// //     } else {
+// //       return NextResponse.json(response.data, { status: response.status });
+// //     }
+// //   } catch (error) {
+// //     console.error("Error sending code to backend:", error);
+// //     return NextResponse.json(
+// //       { error: "Failed to send code to backend" },
+// //       { status: 500 }
+// //     );
+// //   }
+// // }
+
+// // import { NextRequest, NextResponse } from "next/server";
+// // import axios from "axios";
+// // import Cookies from "js-cookie";
+
+// // export async function GET(req: NextRequest) {
+// //   console.log("OAuth2 callback handler:");
+
+// //   const { searchParams } = new URL(req.url);
+// //   const state = searchParams.get("state");
+// //   const code = searchParams.get("code");
+// //   const scopes = searchParams.get("scopes");
+// //   const authuser = searchParams.get("authuser");
+// //   const prompt = searchParams.get("prompt");
+
+// //   console.log("Received query params:", state, code, scopes, authuser, prompt);
+
+// //   if (!code) {
+// //     return NextResponse.json(
+// //       { error: "Authorization code is missing" },
+// //       { status: 400 }
+// //     );
+// //   }
+
+// //   const token = req.cookies.get("auth_token");
+// //   console.log("This is the token:", token);
+// //   if (!token) {
+// //     return NextResponse.json(
+// //       { error: "User token is missing" },
+// //       { status: 401 }
+// //     );
+// //   }
+
+// //   try {
+// //     const response = await axios.post(
+// //       "https://starfish-app-9ezx5.ondigitalocean.app/datasources/gdrive/",
+// //       {
+// //         code,
+// //       },
+// //       {
+// //         headers: {
+// //           Authorization: `Token ${token.value}`,
+// //         },
+// //       }
+// //     );
+
+// //     const data = response.data;
+// //     console.log("Response from backend:", data);
+
+// //     if (response.status >= 200 && response.status < 300) {
+// //       const { username } = response.data.integration["username"];
+// //       const baseUrl = req.nextUrl.origin;
+
+// //       return NextResponse.redirect(
+// //         `${baseUrl}/dataSources?email=${encodeURIComponent(username)}`
+// //       );
+// //     } else {
+// //       return NextResponse.json(response.data, { status: response.status });
+// //     }
+// //   } catch (error) {
+// //     console.error("Error sending code to backend:", error);
+// //     return NextResponse.json(
+// //       { error: "Failed to send code to backend" },
+// //       { status: 500 }
+// //     );
+// //   }
+// // }
+
 // import { NextRequest, NextResponse } from "next/server";
 // import axios from "axios";
 // import Cookies from "js-cookie";
@@ -6,13 +139,7 @@
 //   console.log("OAuth2 callback handler:");
 
 //   const { searchParams } = new URL(req.url);
-//   const state = searchParams.get("state");
 //   const code = searchParams.get("code");
-//   const scopes = searchParams.get("scopes");
-//   const authuser = searchParams.get("authuser");
-//   const prompt = searchParams.get("prompt");
-
-//   console.log("Received query params:", state, code, scopes, authuser, prompt);
 
 //   if (!code) {
 //     return NextResponse.json(
@@ -22,7 +149,8 @@
 //   }
 
 //   const token = req.cookies.get("auth_token");
-//   console.log(token);
+//   console.log("Token from cookies:", token);
+
 //   if (!token) {
 //     return NextResponse.json(
 //       { error: "User token is missing" },
@@ -33,79 +161,12 @@
 //   try {
 //     const response = await axios.post(
 //       "https://starfish-app-9ezx5.ondigitalocean.app/datasources/gdrive/",
-//       {
-//         code,
-//       },
-//       {
-//         headers: {
-// Authorization: `Token ${token.value}`,
-//         },
-//       }
-//     );
-
-//     const data = response.data;
-//     console.log("Response from backend:", data);
-
-//     if (response.status === 200) {
-//       const { email } = response.data;
-
-//       return NextResponse.redirect(
-//         `/dataSources?email=${encodeURIComponent(email)}`
-//       );
-//     } else {
-//       return NextResponse.json(response.data, { status: response.status });
-//     }
-//   } catch (error) {
-//     console.error("Error sending code to backend:", error);
-//     return NextResponse.json(
-//       { error: "Failed to send code to backend" },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-// import { NextRequest, NextResponse } from "next/server";
-// import axios from "axios";
-// import Cookies from "js-cookie";
-
-// export async function GET(req: NextRequest) {
-//   console.log("OAuth2 callback handler:");
-
-//   const { searchParams } = new URL(req.url);
-//   const state = searchParams.get("state");
-//   const code = searchParams.get("code");
-//   const scopes = searchParams.get("scopes");
-//   const authuser = searchParams.get("authuser");
-//   const prompt = searchParams.get("prompt");
-
-//   console.log("Received query params:", state, code, scopes, authuser, prompt);
-
-//   if (!code) {
-//     return NextResponse.json(
-//       { error: "Authorization code is missing" },
-//       { status: 400 }
-//     );
-//   }
-
-//   const token = req.cookies.get("auth_token");
-//   console.log(token);
-//   if (!token) {
-//     return NextResponse.json(
-//       { error: "User token is missing" },
-//       { status: 401 }
-//     );
-//   }
-
-//   try {
-//     const response = await axios.post(
-//       "https://starfish-app-9ezx5.ondigitalocean.app/datasources/gdrive/",
-//       {
-//         code,
-//       },
+//       { code },
 //       {
 //         headers: {
 //           Authorization: `Token ${token.value}`,
 //         },
+//         withCredentials: true,
 //       }
 //     );
 
@@ -113,7 +174,7 @@
 //     console.log("Response from backend:", data);
 
 //     if (response.status >= 200 && response.status < 300) {
-//       const { username } = response.data.integration["username"];
+//       const { username } = response.data.integration;
 //       const baseUrl = req.nextUrl.origin;
 
 //       return NextResponse.redirect(
@@ -133,7 +194,6 @@
 
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
-import Cookies from "js-cookie";
 
 export async function GET(req: NextRequest) {
   console.log("OAuth2 callback handler:");
@@ -148,6 +208,7 @@ export async function GET(req: NextRequest) {
     );
   }
 
+  // Use NextRequest cookies method to get the token
   const token = req.cookies.get("auth_token");
   console.log("Token from cookies:", token);
 
@@ -164,7 +225,7 @@ export async function GET(req: NextRequest) {
       { code },
       {
         headers: {
-          Authorization: `Token ${token.value}`,
+          Authorization: `Token ${token}`,
         },
         withCredentials: true,
       }
