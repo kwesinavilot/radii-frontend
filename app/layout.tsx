0c0cfc2ad56cfefe5adfbaef7f4ed1171904a4a4
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-08-05 15:34:19 +0000

    Add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,7 @@ import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const ibmPlexSerif = IBM_Plex_Serif({
@@ -103,6 +104,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script type="text/javascript">
+          {`
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "nile5ncvyg");
+      
+      `}
+        </Script>
+      </head>
       <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
         <GoogleOAuthProvider clientId="75800942170-6uk0kinmmo3a308dscul4mk72g7uavr9.apps.googleusercontent.com">
           <Providers>{children}</Providers>
